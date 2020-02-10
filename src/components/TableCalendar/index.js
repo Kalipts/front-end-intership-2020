5bcba3bca944dd61e86ad3c675c631005cdb9ba8
Nha Phan <nhaphan0073@gmail.com>
2020-02-10 17:14:41 +0700

    finised table calendar

--- src/components/TableCalendar/index.js
+++ src/components/TableCalendar/index.js
@@ -2,106 +2,129 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 
+function TableSchedule() {
+    return (
+        <div className="item contain">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>
+    )
+}
+
+function TopChedule() {
+    return (
+        <div className="item top">
+        <div className="month">
+        <div className="y-2019"><span className="top-week"> Week 50</span></div>
+    <div className="week">
+        <div className="day0 day mo">
+        <span className="day1">Mon</span>
+        <br/>
+        <span>23</span>
+        </div>
+        <div className="day0 day tu">
+        <span className="day1">Tue</span>
+        <br/>
+        <span>24</span>
+        </div>
+        <div className="day0 day we">
+        <span className="day1">Web</span>
+        <br/>
+        <span>25</span>
+        </div>
+        <div className="day0 day th">
+        <span className="day1">Thu</span>
+        <br/>
+        <span>24</span>
+        </div>
+        <div className="day0 day fr">
+        <span className="day1">Fri</span>
+        <br/>
+        <span>25</span>
+        </div>
+        <div className="day0 day sa">
+        <span>Sat</span>
+        <br/>
+        <span>26</span>
+        </div>
+        <div className="day0 day su">
+        <span>Sun</span>
+        <br/>
+        <span>27</span>
+        </div>
+        </div>
+        </div>
+        </div>
+    )
+}
+
 function  TableCalendar() {
     return (
         <div className="container">
             <div className="item aside1"></div>
             <div className="item aside2">
             </div>
-            <div className="item top">
-                <div className="month">
-                    <div className="y-2019"><span className="top-week"> Week 50</span></div>
-                    <div className="week">
-                        <div className="day0 day mo">
-                            <span className="day1">Mon</span>
-                            <br/>
-                            <span>23</span>
-                        </div>
-                        <div className="day0 day tu">
-                            <span className="day1">Tue</span>
-                            <br/>
-                            <span>24</span>
-                        </div>
-                        <div className="day0 day we">
-                            <span className="day1">Web</span>
-                            <br/>
-                            <span>25</span>
-                        </div>
-                        <div className="day0 day th">
-                            <span className="day1">Thu</span>
-                            <br/>
-                            <span>24</span>
-                        </div>
-                        <div className="day0 day fr">
-                            <span className="day1">Fri</span>
-                            <br/>
-                            <span>25</span>
-                        </div>
-                        <div className="day0 day sa">
-                            <span>Sat</span>
-                            <br/>
-                            <span>26</span>
-                        </div>
-                        <div className="day0 day su">
-                            <span>Sun</span>
-                            <br/>
-                            <span>27</span>
-                        </div>
-                    </div>
-                </div>
+        {/*top*/}
 
-            </div>
-            <div className="item contain">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
+        <TopChedule />
+
+
+        {/*content */}
+
+        <TableSchedule />
+
+
+
+
+
+
         </div>
     )
 }
