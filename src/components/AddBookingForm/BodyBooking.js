a1a9809de38d7002f174325c3af98a89a60fb439
Hoang Cao Thiem <hcaothiem@gmail.com>
2020-03-04 07:53:11 +0700

    Fix, refactor Add booking and fix click outside
    # Conflicts:
    #	src/components/AddBookingForm/BodyBooking.js

--- src/components/AddBookingForm/BodyBooking.js
+++ src/components/AddBookingForm/BodyBooking.js
@@ -1,36 +1,31 @@
 import styled from "styled-components";
-import {CES_GREY_BORDER, CES_GREY_TITLE, CES_ORANGE} from "../../constants/colorTypes";
-
-export const BodyAddBooking = styled.div`
-  margin-top: 10px;
-  height: 340px;
-`;
+import { CES_GREY_BORDER, CES_ORANGE } from "../../constants/colorTypes";
 
 export const TimeRatio = styled.div`
   height: 50px;
   display: flex;
+  margin-top: 10px;
 `;
 
 export const Percentage = styled.div`
   box-sizing: border-box;
-  height: 34px;
+  height: 36px;
   width: 140px;
   border: 1px solid ${CES_ORANGE};
-  border-radius: 16.5px;
-  margin-top: 5px;
-  margin-left: 24.5px;
+  border-radius: 17px;
+  margin-top: 6px;
   display: flex;
   cursor: pointer;
 `;
 
-export const Duratio = styled.div`
+export const Duration = styled.div`
   box-sizing: border-box;
-  height: 34px;
+  height: 36px;
   width: 114px;
   border: 1px solid ${CES_GREY_BORDER};
-  border-radius: 16.5px;
+  border-radius: 17px;
   margin-top: 5px;
-  margin-left: 15px;
+  margin-left: 20px;
   display: flex;
   cursor: pointer;
 `;
@@ -51,16 +46,16 @@ export const Lock = styled.img`
 `;
 
 export const DurationInside = styled.div`
-  margin-top: 7px;
+  margin-top: 9px;
   padding-left: 10px;
-  font-size: 14px;
+  font-size: 15px;
   color: ${CES_GREY_BORDER};
 `;
 
 export const PercentageInside = styled.div`
-  margin-top: 7px;
+  margin-top: 9px;
   padding-left: 10px;
-  font-size: 14px;
+  font-size: 15px;
   color: ${CES_ORANGE};
 `;
 
@@ -68,299 +63,26 @@ export const BookingTime = styled.div`
   height: 50px;
   display: flex;
 `;
-
-export const Start = styled.div`
-  width: 136px;
-  display: flex;
-  flex-direction: column;
-  margin-left: 25px;
-`;
-
-export const StartSpan = styled.div`
-  height: 15px;
-  width: 28px;
-  font-size: 12px;
-  line-height: 15px;
-  color: ${CES_GREY_TITLE};
-`;
-
-export const StartDate = styled.div`
-  display: flex;
-  
-`;
-
-export const StartDatePicker = styled.input.attrs({type:'text'})`
-    height: 18px;
-    width: 80px;
-    color: black;
-    font-family: Muli;
-    font-size: 14px;
-    line-height: 18px;
-    border: none;
-`;
-
-export const DateImage = styled.img`
-  margin-left: 22px;
-`;
-
-export const BottomLineDate = styled.div`
-  height: 2px;
-  width: 136px;
-  transform: scaleY(-1);
-  background-color: #D0D0D0;
-  margin-top: 3px;
-`;
-
-export const End = styled.div`
-  width: 136px;
-  display: flex;
-  flex-direction: column;
-  margin-left: 25px;
-`;
-
-export const EndSpan = styled.div`
-  height: 15px;
-  width: 28px;
-  font-size: 12px;
-  line-height: 15px;
-  color: ${CES_GREY_TITLE};
-`;
-
-export const EndDate = styled.div`
-  display: flex;
-  
-`;
-
-export const EndDatePicker = styled.input.attrs({type:'text'})`
-    height: 18px;
-    width: 80px;
-    color: black;
-    font-family: Muli;
-    font-size: 14px;
-    line-height: 18px;
-    border: none;
-`;
-
 export const Utilization = styled.div`
+  width: 136px;
   display: flex;
   flex-direction: column;
   height: 40px;
-  margin-left: 25px;
-`;
-
-export const UtilizationSpan = styled.div`
-  height: 15px;
-  width: 56px;
-  color: #7B7B7B;
-  font-family: Muli;
-  font-size: 12px;
-  line-height: 15px;
-`;
-
-export const UtilizationPercent = styled.div`
-   height: 18px;
-  width: 39px;
-  color: Black;
-  font-family: Muli;
-  font-size: 14px;
-  line-height: 18px;
+  input {
+    line-height: 18px;
+    color: black;
+    border: none;
+    outline: none;
+    width: 100%;
+    padding: 3px;
+  }
+  & > div {
+    margin-top: 0;
+  }
 `;
 
 export const TotalTime = styled.div`
-    height: 20px;
-    margin-left: 25px;
+  height: 30px;
+  margin-top: 15px;
+  padding: 5px auto;
 `;
-
-export const TotalTimeSpan = styled.div`
-  height: 15px;
-  width: 100px;
-  color: #7B7B7B;
-  font-family: Muli;
-  font-size: 12px;
-  line-height: 15px;
-  margin-top: 10px;
-`;
-
-export const Project = styled.div`
-  height: 55px;
-`;
-
-export const ProjectContain = styled.div`
-  height: 100%;
-  margin-left: 24.5px;
-  margin-right: 18.5px;
-  display: flex;
-  flex-direction: column;
-  box-sizing: border-box;
-
-  border: 1px solid #DEDEDE;
-  border-radius: 2px;
-  background-color: #FFFFFF;
-`;
-
-export const ProjectTopTitle = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-export const ProjectIcon = styled.img`
-  height: 10.55px;
-  width: 12px;
-  margin-left: 15.5px;
-  margin-top: 5px;
-`;
-
-export const ProjectSpan = styled.div`
-  height: 15px;
-  width: 45px;
-  opacity: 0.6;
-  color: #000000;
-  font-family: Muli;
-  font-size: 12px;
-  line-height: 15px;
-  margin-left: 10px;
-  margin-top: 2px;
-`;
-
-export const ProjectBody = styled.div`
-  height: 20px;
-  display: flex;
-  margin-left: 15.5px;
-  margin-top: 5px;
-`;
-
-export const ProjectColor = styled.div`
-  height: 15px;
-  width: 3px;
-  border-radius: 1px;
-  background-color: ${props => props.color || "#F8465C"};
-
-`;
-
-export const ProjectName = styled.div`
-  height: 18px;
-  width: 135px;
-  color: black;
-  font-family: Muli;
-  font-size: 14px;
-  line-height: 18px;
-  margin-left: 5px;
-`;
-
-
-export const ProjectDetails = styled.div`
-  margin-top: 5px;
-`;
-
-export const ContainDetails = styled.div`
-  height: 100%;
-  margin-left: 24.5px;
-  margin-right: 18.5px;
-  display: flex;
-  flex-direction: column;
-  box-sizing: border-box;
-
-  border: 1px solid #DEDEDE;
-  border-radius: 2px;
-  background-color: #FFFFFF;
-
-`;
-export const DetailsTop = styled.div`
-  display: flex;
-
-`;
-
-export const DetailsBottom = styled.div`
-  margin-top: 2px;
-  margin-bottom: 2px;
-   min-height: 5px;
-`;
-
-export const DetailsIcon = styled.img`
-  margin-left: 15.5px;
-  margin-top: 10px ;
-`;
-
-export const DetailsSpan = styled.div`
-    margin-left: 8px ;
-    margin-top: 10px;
-    
-    height: 15px;
-  width: 39px;
-  color: #000000;
-  font-family: Muli;
-  font-size: 12px;
-  line-height: 15px;  
-`;
-
-
-
-
-export const Resource = styled.div`
-  height: 55px;
-  margin-top: 5px;
-`;
-
-export const ResourceContain = styled.div`
-  height: 100%;
-  margin-left: 24.5px;
-  margin-right: 18.5px;
-  display: flex;
-  flex-direction: column;
-  box-sizing: border-box;
-
-  border: 1px solid #DEDEDE;
-  border-radius: 2px;
-  background-color: #FFFFFF;
-`;
-
-export const ResourceTopTitle = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-export const ResourceIcon = styled.img`
-  height: 10.55px;
-  width: 12px;
-  margin-left: 15.5px;
-  margin-top: 5px;
-`;
-
-export const ResourceSpan = styled.div`
-  height: 15px;
-  width: 45px;
-  opacity: 0.6;
-  color: #000000;
-  font-family: Muli;
-  font-size: 12px;
-  line-height: 15px;
-  margin-left: 10px;
-  margin-top: 2px;
-`;
-
-export const ResourceBody = styled.div`
-  height: 20px;
-  display: flex;
-  margin-left: 15.5px;
-  margin-top: 5px;
-`;
-
-export const ResourceAvatar = styled.img`
-   box-sizing: border-box;
-  height: 21px;
-  width: 21px;
-  border: 1px solid #E3E3E3;
-  border-radius: 50%;
-`;
-
-export const ResourceName = styled.div`
-  height: 18px;
-  width: 135px;
-  color: black;
-  font-family: Muli;
-  font-size: 14px;
-  line-height: 18px;
-  margin-left: 5px;
-`;
-
-
