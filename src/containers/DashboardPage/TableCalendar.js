7c244d50f0bd355886762621b52788d11d07c4cc
Hoang Cao Thiem <hcaothiem@gmail.com>
2020-03-04 07:53:11 +0700

    Fix, refactor Add booking and fix click outside

--- src/containers/DashboardPage/TableCalendar.js
+++ src/containers/DashboardPage/TableCalendar.js
@@ -50,6 +50,7 @@ function TableCalendar({ startDay, endDay }) {
   const renderCellsInCalendar = indexResource => {
     const days = new Array(numberOfDay).fill(1).map((item, i) => {
       const dateInCell = moment(startDay.toString()).add(i, 'days');
+      console.log(`day in cell: ${dateInCell}`);
       const bookingDateWithResource = renderBooking(dateInCell, indexResource);
       const weekDayName = dateInCell.format('ddd');
       const isWeekend = weekDayName === 'Sun' || weekDayName === 'Sat';
