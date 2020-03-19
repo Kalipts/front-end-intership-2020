0fc561e40625afdae1a53851ccdd64fd4374ddb6
Nha Phan <nhaphan0073@gmail.com>
2020-03-19 02:42:43 +0000

    Refactor hover

--- src/constants/colorTypes.js
+++ src/constants/colorTypes.js
@@ -1,7 +1,8 @@
-export const WHITE = "#FFFFFF";
+export const WHITE = '#FFFFFF';
 
-export const CES_ORANGE = "#F15927";
-export const CES_GREY = "#DDDDDD";
-export const CES_GREY_BORDER = "#9A9A9A";
-export const CES_GREY_TITLE = "#7B7B7B";
-export const CES_ORANGE_HOVER = "rgb(241, 89, 39, 0.09)";
+export const CES_ORANGE = '#F15927';
+export const CES_GREY = '#DDDDDD';
+export const CES_GREY_BORDER = '#9A9A9A';
+export const CES_GREY_TITLE = '#7B7B7B';
+export const CES_ORANGE_HOVER = 'rgb(241, 89, 39, 0.09)';
+export const CES_GREY_HOVER = '#D8D8D8';
---
+++ src/containers/DashboardPage/RowCalendar.js
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react';
+import RowBookingView from './TableCalendar/Style/RowBookingView';
+import useCellsInCalendar from './TableCalendar/useCellsInCalendar';
+import { CalendarContext } from '../../context/Calendar';
+import CellInCalendar from './CellCalendar';
+
+const renderCellsInCalendar = (resource, row, indexResource) => (
+  <CellInCalendar resource={resource} row={row} indexResource={indexResource} />
+);
+
+const RenderRowsInCalendar = () => {
+  const calendarContext = useContext(CalendarContext);
+  const { getMaxTotalOverlapBooking, startDay, endDay } = calendarContext;
+
+  const { cells } = useCellsInCalendar(startDay, endDay);
+
+  const renderCells = cells.map((row, indexResource) => {
+    const { contentResource, resource } = row;
+    const days = renderCellsInCalendar(
+      resource,
+      contentResource,
+      indexResource,
+    );
+
+    return (
+      <RowBookingView
+        key={resource._id}
+        overlapBooking={getMaxTotalOverlapBooking(resource._id)}
+      >
+        {days}
+      </RowBookingView>
+    );
+  });
+  return renderCells;
+};
+
+export default RenderRowsInCalendar;
