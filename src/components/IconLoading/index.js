5eb8ad5043b03b83436a40dda2088a8b3b5e8686
Nha Phan <nhaphan0073@gmail.com>
2020-03-26 15:53:12 +0700

    screen loading

---
+++ src/components/IconLoading/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.css';
+export default function LoadingIcon() {
+  return (
+    <div className="lds-spinner">
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+}
